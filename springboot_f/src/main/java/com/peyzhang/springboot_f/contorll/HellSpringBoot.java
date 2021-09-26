2820926a79bd5025cdebd911e120ff6f1a62d057
CISCO\peyzhang <peyzhang@cisco.com>
2021-09-26 19:22:50 +0800

    demo01

--- springboot_f/src/main/java/com/peyzhang/springboot_f/contorll/HellSpringBoot.java
+++ springboot_f/src/main/java/com/peyzhang/springboot_f/contorll/HellSpringBoot.java
@@ -1,4 +1,4 @@
-package com.peyzhang.springboot_f;
+package com.peyzhang.springboot_f.contorll;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
